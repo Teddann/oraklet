8b8222d34d51a27d7bd5a7667464ef8394cdb25e
ted <tedgra@edu.umea.se>
2026-09-03 11:47:47 +0200

    tor 03/9

--- Slumpigt.py
+++ Slumpigt.py
@@ -3,10 +3,10 @@ import random
 gissning = int(input("Gissa vilken siffra jag tänker på. "))
 
 nummer = random.randint (0,9)
-
-if nummer == gissning: 
-    print("Du hade rättttttttttttttttttttttt!")
+while gissning != nummer:
+    if nummer != gissning: 
+        print("Du hade fel!")
+    gissning = int(input("gissa igen apa! "))
 else: 
-
-  
-
+    print("du hade jätte rätt, hahahaha!")
+         
