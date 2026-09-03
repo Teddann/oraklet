2462f73651fbfd5ada73d577627b1daa03e7c003
ted <tedgra@edu.umea.se>
2026-09-03 11:03:10 +0200

    tis 01/09

---
+++ Slumpigt.py
@@ -0,0 +1,10 @@
+import random
+
+gissning = input("Gissa vilken siffra jag tänker på. ")
+
+nummer = random.randint(0, 9)
+
+if nummer == gissning: 
+    
+    print("Du hade rätt!")
+else: print("Du hade fel.")
