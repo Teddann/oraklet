cd315d73848bbc9fe9b5f900b48374cd27e475eb
ted <tedgra@edu.umea.se>
2026-09-08 11:50:02 +0200

    tis 08/09 spelet funka

--- Ut tors 3/9.py
+++ Ut tors 3/9.py
@@ -1,17 +1,93 @@
 import random
-namn = input("Hej, vad heter du?")
-print(f"Hej {namn}! du ska få så nära 10 som möjligt men inte gå över. Jag slår första kastet och du får    bestämma om du vill slå igen.")
+namn = input("Hej, vad heter du? ")
+print(f"Hej {namn}! du ska få så nära 10 som möjligt men inte gå över. Jag slår första kastet och du får bestämma om du vill slå igen.")
+
+
 tärning = random.randint (1,6)
 tarning = random.randint (1,6)
 tarnung = random.randint (1,6)
-
+tärnung = random.randint (1,6)
+tirnung = random.randint (1,6)
 svar = input(f"Du fick {tärning} vill då slå igen? ")
 
-if svar == "ja":
-    svara = input(f"Du fick {tarning}, nu har du {tärning + tarning}. Vill du fortsätta slå? ")
+
+
+if svar != "ja": 
+    print(f"Du stannade på {tärning}. Bra jobbat, vill du spela igen? ")
+    exit()
 else:
-    svara = print(f"Du stannade på {tärning}. Bra jobbat, vill du spela igen? ")
+    print(f"Du fick {tarning}, nu har du {tärning + tarning}. ") 
+
+if tärning + tarning == 10:
+    print("snyggt jobbat, du vann!")
+    print("vill du spela igen")
+    exit()
+else:
+    print()
+    
+if tärning + tarning > 10:
+    print("du blev tjock")
+    exit()
+else:
+    svara = input("Vill du fortsätta slå? ")
+
+
+
 if svara == "ja":
-     svara = input(f"Du fick {tarnung}, nu har du {tärning + tarning + tarnung}. Vill du fortsätta slå? ")
+    print(f"Du fick {tarnung}, nu har du {tärning + tarning + tarnung}. ")
 else:
-    print(f"Du stannade på {tärning + tarning}. ")
+    print(f"Du stannade på {tärning + tarning}, vill du spela igen? ")
+    exit()
+
+if tärning + tarning + tarnung == 10:
+    print("snyggt jobbat, du vann!")
+    print("vill du spela igen")
+    exit()
+else:
+    print()
+
+if tärning + tarning + tarnung > 10:
+    print("du blev tjock")
+    exit()
+else:
+    svarade = input("Vill du fortsätta slå?")
+
+
+if svarade == "ja":
+    print(f"Du fick {tärnung}, nu har du {tärning + tarning + tarnung + tärnung}. ")
+else:
+    print(f"Du stannade på {tärning + tarning + tarnung}, vill du spela igen? ")
+    exit()
+
+if tärning + tarning + tarnung + tärnung == 10:
+    print("snyggt jobbat, du vann!")
+    print("vill du spela igen")
+    exit()
+else:
+    print()
+
+if tärning + tarning + tarnung + tärnung > 10:
+    print("du blev tjock")
+    exit()
+else:
+    svarat = input("Vill du fortsätta slå?")
+
+if svarat == "ja":
+    print(f"Du fick {tirnung}, nu har du {tärning + tarning + tarnung + tärnung + tirnung}. ")
+else:
+    print(f"Du stannade på {tärning + tarning + tarnung + tärnung}. Bra jobbat, vill du spela igen? ")
+    exit()
+
+if tärning + tarning + tarnung + tärnung + tirnung == 10:
+    print("snyggt jobbat, du vann!")
+    print("vill du spela igen")
+    exit()
+else:
+    print()
+
+if tärning + tarning + tarnung + tärnung + tirnung > 10:
+    print("du blev tjock")
+    exit()
+else:
+    svarate = input("Vill du fortsätta slå?")
+
